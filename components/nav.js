0f756cea6e490b643793e02893d9d8b988fb4dec
itc-s23015 <s23015@std.it-college.ac.jp>
2023-12-14 08:40:47 +0000

    saisyo

---
+++ components/nav.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function Nav () {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/blog'>
+            <a>Blog</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
