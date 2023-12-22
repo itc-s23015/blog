507c3c1dea557429bdf6990c924dc61d10daf592
itc-s23015 <s23015@std.it-college.ac.jp>
2023-12-22 00:16:24 +0000

    chapter5

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,17 @@
+import Container from 'components/container'
 import Logo from 'components/logo'
+import Social from 'components/social'
+import styles from 'styles/footer.module.css'
 
 export default function Footer () {
   return (
-    <footer>
-      <Logo />
+    <footer className={styles.wrapper}>
+      <Container>
+        <div className={styles.flexContainer}>
+          <Logo />
+          <Social />
+        </div>
+      </Container>
     </footer>
   )
 }
